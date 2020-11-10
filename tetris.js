032bc43de332c081a19720f47b2d68d624c691db
alift99 <67675036+alift99@users.noreply.github.com>
2020-11-10 16:37:25 +0800

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -51,26 +51,6 @@ function swapPiece(){
     heldPiece = temp;
 }
 
-// CONTROL the piece
-
-//document.addEventListener("keydown",CONTROL);
-
-
-function CONTROL(event){
-    if(event.keyCode == 37){
-        activePiece.moveLeft();
-        dropStart = Date.now();
-    }else if(event.keyCode == 38){
-        activePiece.rotate();
-        dropStart = Date.now();
-    }else if(event.keyCode == 39){
-        activePiece.moveRight();
-        dropStart = Date.now();
-    }else if(event.keyCode == 40){
-        activePiece.moveDown();
-    }
-}
-
 
 // drop the piece every 1sec
 
